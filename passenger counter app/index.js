aea6b055eabff412f08406733a58e6f1adfcb32b
mimihadid <93333942+mimihadid@users.noreply.github.com>
2022-11-17 14:03:34 +0100

    passenger app changes

--- passenger counter app/index.js
+++ passenger counter app/index.js
@@ -1,17 +1,19 @@
 // document.getElementById("count-el").innerText = 5;
 //--------------------------------------------------------
-// select text and ~ cmd + k + c ~ to make it a comment
-//--------------------------------------------------------
-
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function save() {
-  console.log(count);
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
-save();
+
+console.log("Let's count people on the subway!");
