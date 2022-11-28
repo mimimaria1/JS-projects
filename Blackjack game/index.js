c8302f7e3560632be2e9b31a25de0d63bfd594d9
mimihadid <93333942+mimihadid@users.noreply.github.com>
2022-11-28 14:45:22 +0100

    a lot of changes

--- Blackjack game/index.js
+++ Blackjack game/index.js
@@ -1,20 +1,40 @@
 let firstCard = getRandomCard();
 let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+let player = {
+  name: "Maria",
+  chips: 375,
+};
+
+playerEl.textContent = player.name + ": €" + player.chips;
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
 function getRandomCard() {
-  return Math.floor(Math.random() * 13);
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function renderGame() {
@@ -36,9 +56,16 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  console.log(cards);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
+  }
 }
+
+// In this lesson I learned;
+// Arrays, objects, booleans, if/else statements,
+// comparison operators, logical operators, for loops,
+// the Math object and return statements
--- passenger counter app/NOTES.js
+++ passenger counter app/NOTES.js
@@ -314,9 +314,150 @@
 //     return randomNumber;
 //   }
 //   console.log(rollDice());
+//  return Math.floor(Math.random() * 13) + 1; ===> generates numbers from 1 - 13
 //--------------------------------------------------------
+// Logical operators:
+
+// let hasCompletedCourse = true
+// let givesCertificate = true
+// if(hasCompletedCourse === true) {
+//     if(givesCertificate === true ) {
+//         generatesCertificate()
+//     }
+// }
+// OR you can also write is like this:
+// if(hasCompletedCourse === true && givesCertificate === true) {
+//     generatesCertificate()
+// }
+// the values of let should both be true to generate the certificate
+
+// OR you can also write it like this:
+// if(true && true) {
+//     generatesCertificate()
+// }
+
+// function generateCertificate() {
+//     console.log("Generating certificate....")
+// }
+//--------------------------------------------------------
+// let hasSolvedChallenge = false
+// let hasHintsLeft = false
+
+// if(hasSolvedChallenge === false && hasHintsLeft === false) {
+//   showSolution()
+// }
+
+// function showSolution() {
+//   console.log("Show the solution..")
+// }
+//--------------------------------------------------------
+// let likesDocumentaires = true;
+// let likesStartups = false;
+
+// if (likesDocumentaires === true || likesStartups === false) {
+//   recommendedMovie();
+// }
+
+// function recommendedMovie() {
+//   console.log("Hey, check out this new movie!");
+// }
+//--------------------------------------------------------
+// let course = {
+//     title: "Learn JS for free",
+//     lessons: 16,
+//     creator: "Maria",
+//     length: 63,
+//     level: "beginner",
+//     isFree: true,
+//     tags: ["html, css"]
+// }
+
+// console.log(course.length)
+// Will gives us 63, thats how you look inside the object
 
 //--------------------------------------------------------
+// Make an object with an Boolean, string, number and an array:
+
+// let castle = {
+//     title: "Come stay at our Airbnb",
+//     price: 210,
+//     isSuperHost: true,
+//     images: ["img/apartment1.png", "img/apartment2.png"]
+// }
+//--------------------------------------------------------
+// OBJECTS AND FUNCTIONS <------
+// let person = {
+//   name: "Maria",
+//   age: 30,
+//   country: "the Netherlands",
+// };
+
+// function logData() {
+//   console.log(
+//     person.name +
+//       " is " +
+//       person.age +
+//       " years old and lives in " +
+//       person.country
+//   );
+// }
+// logData();
+//--------------------------------------------------------
+// IF ELSE STATEMENTS  <------
+// let age = 15;
+
+// if (age < 6) {
+//   console.log("free");
+// } else if (age < 18) {
+//     console.log("child discount")
+// } else if (age < 27) {
+//     console.log("student discount")
+// } else if (age < 67){
+//     console.log("full price")
+// } else (age > 67) {
+//     console.log("senior citizen discount")
+// }
+//--------------------------------------------------------
+// LOOPS AND ARRAYS  <------
+// console.log("The 5 biggest countries in the world")
+// let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
+// for (let i = 0; i < largeCountries.length; i++) {
+//   console.log("$ " + largeCountries[i]);
+// }
+//--------------------------------------------------------
+// PUSH, POP, UNSHIFT, SHIFT  <------
+// unshift() and shift()    works in the beginning of the array
+// push() and pop()         works in the end of the array
+// shift()                  removes first element
+// unshift()                adds first elements
+// pop()                    removes last element in array
+// push()                   adds last element in array
+// let largeCountries = ["Monaco", "India", "USA", "Indonesia", "Netherlands"];
+
+// largeCountries.pop();
+// largeCountries.push("Pakistan");
+
+// largeCountries.shift();
+// largeCountries.unshift("China");
+
+// console.log(largeCountries);
+
+//--------------------------------------------------------
+// LOGICAL OPERATORS  <------
+// let dayOfMonth = 13;
+// let weekday = "Friday";
+
+// if (dayOfMonth === 13 && weekday === "Friday") {
+//   console.log("👻");
+// }
+
+// let hands = ["rock", "paper", "scissor"];
+
+// function getHand() {
+//   let randomIndex = Math.floor(Math.random() * 3);
+//   return hands[randomIndex];
+// }
+// console.log(getHand());
 
 //--------------------------------------------------------
 
