426a6dbf09489df6202960aef368d1f524fa9110
mimihadid <93333942+mimihadid@users.noreply.github.com>
2022-11-29 15:49:36 +0100

    html css changes

--- passenger counter app/NOTES.js
+++ passenger counter app/NOTES.js
@@ -460,7 +460,19 @@
 // console.log(getHand());
 
 //--------------------------------------------------------
+// let fruit = ["🍏 ", "🍏 ", "🍊", "🍏 ", "🍊 ", "🍊 ", "🍏 "]
+// let appleShelf = document.getElementById("apple-shelf")
+// let orangeShelf = document.getElementById("orange-shelf")
 
+// function sortFruit() {
+//     for (let i = 0; i < fruit.length; i++) {
+//         if (fruit[i] === "🍏 ") {
+//             appleShelf.textContent += "🍏"
+//         } else if (fruit[i] === "🍊") {
+//             orangeShelf.textContent += "🍊"
+//         }
+//     }
+// }
 //--------------------------------------------------------
 
 //--------------------------------------------------------
