99210b93f53ee0e18d9a0ed78eb6d5ba10418291
mimihadid <93333942+mimihadid@users.noreply.github.com>
2022-12-01 15:48:44 +0100

    changes

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,18 +1,18 @@
-let myLeads = ["www.royalexclusif.com", "www.cz.nl", "www.pidz.nl"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
 
-let listItems = "";
-for (let i = 0; i < myLeads.length; i++) {
-  listItems += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
 }
-
-ulEl.innerHTML = listItems;
-
-console.log(listItems);
