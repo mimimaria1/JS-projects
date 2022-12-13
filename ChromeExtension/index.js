5f5dd02c4e94aa30265b105e26f9042e9d80713f
mimihadid <93333942+mimihadid@users.noreply.github.com>
2022-12-13 14:35:34 +0100

    localstorage

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -3,6 +3,8 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+localStorage.setItem("");
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
