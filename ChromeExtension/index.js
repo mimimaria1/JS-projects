74cc92fb578a8468c38fd7638b55ad93a44cb680
mimihadid <93333942+mimihadid@users.noreply.github.com>
2022-12-29 21:34:05 +0100

    html button + js tabs

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -4,13 +4,26 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+const tabs = [{ url: "https://www.royal-exclusif.com/ " }];
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+tabBtn.addEventListener("click", function () {
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
 
 function render(leads) {
   let listItems = "";
