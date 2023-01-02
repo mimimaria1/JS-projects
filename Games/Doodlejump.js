da2f0c0c6ee1947cb2b9a3d806bd5e298fa4fa6b
mimihadid <93333942+mimihadid@users.noreply.github.com>
2023-01-02 20:00:50 +0100

    js

--- Games/Doodlejump.js
+++ Games/Doodlejump.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let platforms = [];
   let upTimerId;
   let downTimerId;
+  let isJumping = true;
 
   function createDoodler() {
     grid.appendChild(doodler);
@@ -53,6 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function jump() {
     clearInterval(downTimerId);
+    isJumping = true;
     upTimerId = setInterval(function () {
       doodlerBottomSpace += 20;
       doodler.style.bottom = doodlerBottomSpace + "px";
@@ -64,12 +66,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function fall() {
     clearInterval(upTimerId);
+    isJumping = false;
     downTimerId = setInterval(function () {
       doodlerBottomSpace -= 5;
       doodler.style.bottom = doodlerBottomSpace + "px";
       if (doodlerBottomSpace <= 0) {
         gameOver();
       }
+      platforms.forEach((platform) => {
+        if (
+          doodlerBottomSpace >= platform.bottom &&
+          doodlerBottomSpace <= platform.bottom + 15 &&
+          doodlerLeftSpace + 60 >= platform.left &&
+          doodlerLeftSpace <= platform.left + 85 &&
+          !isJumping
+        ) {
+          console.log("landed");
+          jump();
+        }
+      });
     }, 30);
   }
 
