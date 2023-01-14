517d2f7eb772450192eeaf64f0d6f7ec67af1b66
mimihadid <93333942+mimihadid@users.noreply.github.com>
2023-01-14 12:47:22 +0100

    js

--- FlappyBird/Flappy.js
+++ FlappyBird/Flappy.js
@@ -5,8 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let birdLeft = 220;
   let birdBottom = 100;
-
+  let gravity = 2;
   function startGame() {
+    birdBottom -= gravity;
     bird.style.bottom = birdBottom + "px";
+    bird.style.left = birdLeft + "px";
   }
+  let timerId = setInterval(startGame, 20);
 });
