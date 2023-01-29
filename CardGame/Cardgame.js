d17fa127672c2eb647e00a89e58e2bf0231f7412
mimihadid <93333942+mimihadid@users.noreply.github.com>
2023-01-29 12:02:53 +0100

    js

--- CardGame/Cardgame.js
+++ CardGame/Cardgame.js
@@ -1,6 +1,14 @@
-const cardObjectDefinitions = {
-    {id:1, imagePath: "./images/"}
-    {id:2, imagePath: "/"}
-    {id:3, imagePath: "/"}
-    {id:4, imagePath: "/"}
+const cardObjectDefinitions = [
+  { id: 1, imagePath: "/images/card-KingHearts.png" },
+  { id: 2, imagePath: "/images/card-JackClubs.png" },
+  { id: 3, imagePath: "/images/card-QueenDiamonds.png" },
+  { id: 4, imagePath: "/images/card-AceSpades.png" },
+];
+
+function createCard(cardItem) {
+  const cardElem = document.createElement("div");
+}
+
+function createElement(elemType) {
+  return document.createElement(elemType);
 }
