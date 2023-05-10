81637037868f057fa7bcfb94ffa028b17d25a1b2
mimihadid <93333942+mimihadid@users.noreply.github.com>
2023-05-10 16:25:37 +0200

    array

--- basic-tetris/tetris.js
+++ basic-tetris/tetris.js
@@ -1,1 +1,5 @@
 document.addEventListener("DomContentLoaded", () => {});
+
+const grid = document.querySelector(".grid");
+let squares = Array.from(document.querySelectorAll(".grid div"));
+const width = 10;
