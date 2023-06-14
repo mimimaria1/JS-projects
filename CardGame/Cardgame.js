8b06498777bf4e141a6f49e943b6a7859fe4e0ba
mimihadid <93333942+mimihadid@users.noreply.github.com>
2023-06-14 22:03:16 +0200

    js

--- CardGame/Cardgame.js
+++ CardGame/Cardgame.js
@@ -28,7 +28,7 @@ const currentGameStatusElem = document.querySelector(".current-status");
 const scoreContainerElem = document.querySelector(.header-score-container)
 const scoreElem = document.querySelector('.score')
 const roundContainerElem = document.querySelector('.header-round-container')
-const roundElem = document.querySelector()
+const roundElem = document.querySelector('.round')
 
 const winColor = "green";
 const loseColor = "red";
