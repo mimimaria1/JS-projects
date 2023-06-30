63b328d4571ac8930e27d69add3294df2bbd9c03
mimihadid <93333942+mimihadid@users.noreply.github.com>
2023-06-30 13:46:38 +0200

    eventlistener

--- CardGame/Cardgame.js
+++ CardGame/Cardgame.js
@@ -303,7 +303,7 @@ function createCard(cardItem) {
   initializeCardPositions(cardElem);
 }
 function attachClickEventHandlerToCard(card){
-  
+  card.addEventListener('click')
 }
 
 function initializeCardPositions(card) {
